03d8b774047d7abe540fedf6c298a23ee04b5ee8
Joseph <musilijose618@gmail.com>
2025-02-22 15:26:14 +0300

    Authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,18 @@
 from . import views
 from django.urls import path,include
-from .views import list_books ,LibraryDetailView
+from .views import SingUpView,list_books ,LibraryDetailView,TemplateView,LoginView ,LogoutView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("",views.list_books,name="list_books")
+    path('library_details/',LibraryDetailView.as_views(),name='library_details'),
+    path("register",SingUpView.as_view(),name="templates/registration/register"),
+    path("",views.list_books,name="list_books"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile/',
+             TemplateView.as_view(template_name='accounts/profile.html'),
+             name='profile'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/',LogoutView.as_View(), name='logout')
+    
     
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,18 @@
-from django.views.generic.detail import DetailView 
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views.generic.detail import DetailView 
 from .models import Library,Book
 
 
 
 
-# Create your views here.
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
+
 def list_books(request):
     books = Book.objects.all()
 
@@ -20,9 +27,3 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = Book.objects.filter(library=self.object)
         return context
-    
-    
-
-
-
-
