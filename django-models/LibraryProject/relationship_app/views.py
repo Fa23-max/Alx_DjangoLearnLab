2d4a44f9e9dac74bc65eff5a0e8a61769663633d
Joseph <musilijose618@gmail.com>
2025-02-22 10:59:25 +0300

    relationship app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.detail import DetailView 
 from django.shortcuts import render
 from .models import Library,Book
-from .views import list_books
+from .views import LibraryDetailView
 
 
 
