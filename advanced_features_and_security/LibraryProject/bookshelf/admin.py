05e00f4402b95d2f22bd80bb70d65f5e247ba0fa
Joseph <musilijose618@gmail.com>
2025-03-02 11:51:09 +0300

    modify user model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser
 
 class Bookadmin(admin.ModelAdmin) :
     list_filter = ("title","author","publication_year")
     search_fields = ("title","author","publication_year")
+class CustomUserAdmin(admin.ModelAdmin):
+    ...
 
-
+    
 # Register your models here.
 admin.site.register(Book,Bookadmin)
+admin.site.register(CustomUser,CustomUserAdmin)
 
 
