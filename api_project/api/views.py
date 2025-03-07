9a89733e01ecc4cf51706ca93f08c9323fa35f03
Joseph <musilijose618@gmail.com>
2025-03-07 07:31:34 +0300

    create first api end point

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+def BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        name_filter = self.request.query_params.get('name', None)
+        if name_filter is not None:
+            queryset = queryset.filter(name__icontains=name_filter)
+        return queryset
+    
