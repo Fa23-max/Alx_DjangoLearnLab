22c381cb76d17a4ed43306555c3d7d46a15f2a7f
Joseph <musilijose618@gmail.com>
2025-03-07 09:59:20 +0300

    implement authentication and permissions

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
+    'rest_framework.authentication.TokenAuthentication'
     'rest_framework',
     'api',
 ]
