09242b9b64c12835493755db2064887446d2c90f
Fa23-max <musilijose618@gmail.com>
2025-03-16 13:17:25 +0300

    create filters, search and odering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from .models import Book ,Author
 from .serializers import BookSerializer ,AuthorSerializer
 from rest_framework import generics , filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework ,DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ListView(generics.ListAPIView):
