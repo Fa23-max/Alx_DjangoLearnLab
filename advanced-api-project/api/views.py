5577054d48f10cafa17b5262b4f1358cf556a612
Fa23-max <musilijose618@gmail.com>
2025-03-16 13:08:03 +0300

    create filtering,serching and odering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,21 +1,24 @@
 from .models import Book ,Author
 from .serializers import BookSerializer ,AuthorSerializer
-from rest_framework import generics 
+from rest_framework import generics , filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+    ordering_fields =['title','publiation_year']
+    search_fields = ['title','author_author_name']
+    filterset_fields = ['title','author','publication_year']
 
 class DetailViews(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class CreateView(generics.CreateAPIView ):
-
-    permission_classes = (IsAuthenticatedOrReadOnly)
+   # permission_classes = (IsAuthenticatedOrReadOnly)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
