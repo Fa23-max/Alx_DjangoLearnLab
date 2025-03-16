938bd43d5d298f29c52783b0961d9b43d3fd248d
Fa23-max <musilijose618@gmail.com>
2025-03-16 13:38:42 +0300

    create tests in django

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Book
 
-# Create your tests here.
+class BookTests(APITestCase):
+    def test_create_Book(self):
+        """
+        Ensure we can create a new Book object.
+        """
+        url = reverse('ListBooks')
+        data = {'name': 'DabApps'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertEqual(Book.objects.get().name, 'DabApps')
