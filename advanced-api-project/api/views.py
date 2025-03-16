1a651b5b8e6077c5761ee3e443105d4114007ad0
Fa23-max <musilijose618@gmail.com>
2025-03-16 11:59:14 +0300

    Create custom views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from .models import Book ,Author
 from .serializers import BookSerializer ,AuthorSerializer
 from rest_framework import generics 
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly , IsAuthenticated
 
 
 
