e8825163381df200cd806cf16bb6b7fd68fd7216
Fa23-max <musilijose618@gmail.com>
2025-03-18 12:58:05 +0300

    Create a django blog app

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name="registration/login.html"),name="login"),
     path('logout/',auth_views.LogoutView.as_view() ,name ="logout"),
     path('',views.index, name="home"),
-    path('profile',views.profile, name="profile"),
+    path('profile/',views.profile, name="profile"),
 
 
 ]
