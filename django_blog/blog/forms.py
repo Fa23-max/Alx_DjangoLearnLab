28fc395eb55c0effee62adfd67b5cd53ad8c0516
Fa23-max <musilijose618@gmail.com>
2025-03-22 10:31:41 +0300

    create a django blog app

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,17 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.models import User
 from .models import post , Comment
+from taggit.forms import TagWidget
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = post
+        fields = ('title', 'tags',)
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
