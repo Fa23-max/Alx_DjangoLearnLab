97949b3a852c92bc128790b97dd23b89c163b473
Fa23-max <musilijose618@gmail.com>
2025-03-29 11:46:22 +0300

    create a django social app

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('register/',views.RegisterUserView.as_view(),name="register"),
     path('login/',views.LoginUserView.as_view(),name="login"),
     path('profile',views.RetrieveUserView.as_view(),name="profile"),
+    # path('unfollow/<int:user_id>/')
+    # path(follow/<int:user_id>)
+
 ]
