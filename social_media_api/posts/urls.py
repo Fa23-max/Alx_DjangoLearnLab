5087a4e696ad866973d35a3f1c4e0d936a9a7055
Fa23-max <musilijose618@gmail.com>
2025-03-29 12:07:46 +0300

    create a django social app

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('post/',views.PostViewSet.as_view(),name="post"),
     path('comment/',views.CommentViewSet.as_view(),name="comment"),
     #"feed/"
+    #"<int:pk>/like/"
+    #"<int:pk>/unlike/"
 ]
